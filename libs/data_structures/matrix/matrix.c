5b69338e32066702159d1c37b3dc2d49f9424c20
xddretro <daniil.a.komarov.05@gmail.com>
2024-05-17 23:50:32 +0300

    commit of create

---
+++ libs/data_structures/matrix/matrix.c
@@ -0,0 +1,3 @@
+//
+// Created by ReTro on 17.05.2024.
+//
---
+++ libs/data_structures/matrix/matrix.h
@@ -0,0 +1,8 @@
+//
+// Created by ReTro on 17.05.2024.
+//
+
+#ifndef UNTITLED3_MATRIX_H
+#define UNTITLED3_MATRIX_H
+
+#endif //UNTITLED3_MATRIX_H
