ce1549138a3fc8cbf6e7b23fdbbecc07265ac2d4
xddretro <daniil.a.komarov.05@gmail.com>
2024-05-18 22:48:26 +0300

    some of matrix.c

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -1,3 +1,36 @@
-//
-// Created by ReTro on 17.05.2024.
-//
+#include <stdio.h>
+#include <malloc.h>
+#include <memory.h>
+#include <stdbool.h>
+#include "matrix.h"
+
+void swap(int *x, int *y) {
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+
+matrix getMemMatrix(int nRows, int nCols) {
+    int **values = (int **) malloc(sizeof(int*) * nRows);
+    for (int i = 0; i < nRows; i++)
+        values[i] = (int *) malloc(sizeof(int) * nCols);
+    return (matrix){values, nRows, nCols};
+}
+
+matrix *getMemArrayOfMatrices(int nMatrices,
+                              int nRows, int nCols) {
+    matrix *ms = (matrix*) malloc(sizeof(matrix) * nMatrices);
+    for (int i = 0; i < nMatrices; i++)
+        ms[i] = getMemMatrix(nRows, nCols);
+    return ms;
+}
+
+void freeMemMatrix(matrix *m) {
+    for (int i = 0; i < m->n_rows; i++)
+        free(m->values[i]);
+
+    free(m->values);
+    m->values = NULL;
+    m->n_rows = 0;
+    m->n_cols = 0;
+}
